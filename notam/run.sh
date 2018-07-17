34d2fda55fef6d8f0320d1cb011473a2787cd8bc
kalistratovka <kalistratovka@nis-glonass.ru>
2018-07-17 23:35:06 +0300

    add report

--- notam/run.sh
+++ notam/run.sh
@@ -3,3 +3,4 @@
 pytest -vvrxXs tests/test_notam.py -s --alluredir report #| grep -v 'charsetgroupprober.py' #> test.log
 allure-2.6.0/bin/allure generate -o tests/allure -- report --clean
 allure-2.6.0/bin/allure serve report
+
